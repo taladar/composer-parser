7006c87a190d53f9e717a037114edac2939e97ba
Matthias Hörmann <hoermann@saltation.de>
2022-02-02 15:15:43 +0100

    fix indicated update requirement; bump version to 0.1.2; changelog

--- src/lib.rs
+++ src/lib.rs
@@ -168,9 +168,9 @@ pub fn outdated(
     }
 
     let update_requirement = if output.status.success() {
-        IndicatedUpdateRequirement::UpdateRequired
-    } else {
         IndicatedUpdateRequirement::UpToDate
+    } else {
+        IndicatedUpdateRequirement::UpdateRequired
     };
 
     let json_str = from_utf8(&output.stdout)?;
